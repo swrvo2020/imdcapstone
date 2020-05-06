e936fc6c74cf172a5a8a54421d406e3683b9bca4
John Kim <jj61397@gmail.com>
2020-05-05 23:19:56 -0700

    Robust Logic for Scene Swap
    + Created SceneData object containing all the scene info (img source, sceneTitle, sceneDesc, sceneList)
    + Broke down code into different funcitons
    + commented a lot of shit

--- website/action.js
+++ website/action.js
@@ -1,26 +1,75 @@
-var test = document.querySelector('.change-picture');
+// Select image change button
+const selectImgChange = document.querySelector('.change-button');
 
-var scenes = ["url('/assets/car.jpg')", "url('/assets/tesla.jpg')", "url('assets/tesla-1.jpg')"];
-var sceneLength = scenes.length;
-var imageSwitch = 0;
+// Query select background img tag
+const selectBG = document.querySelector('#background-img');
 
-var sceneLocation = (imageSwitch + 1) + "/" + sceneLength;
+// Query select scene info
+const selectSceneNumber = document.querySelector('.scene-number');
+const selectSceneTitle = document.querySelector('.scene-title');
+const selectSceneDesc = document.querySelector('.scene-description');
 
-function changePicture() {
-  console.log('clicked');
+// Number of scenes
+const sceneLength = 6;
 
-  document.querySelector('#background-img').style.backgroundImage = scenes[imageSwitch];
-  document.querySelector('.scene-number').innerHTML = sceneLocation;
 
-  if (imageSwitch >= (sceneLength - 1)) {
-    console.log("imageScene restarts at " + imageSwitch);
-    imageSwitch = 0;
-    sceneLocation = (imageSwitch + 1) + "/" + sceneLength;
-  } else {
-    console.log("imageScene is at " + imageSwitch);
-    imageSwitch++;
-    sceneLocation = (imageSwitch + 1) + "/" + sceneLength;
-  };
+// SceneData Constructor contains everything that needs to change per scene
+function SceneData(sceneList, sceneTitle, sceneDesc, imgSource) {
+  this.sceneList = sceneList;
+  this.sceneTitle = sceneTitle;
+  this.sceneDesc = sceneDesc;
+  this.imgSource = imgSource;
+
+  this.getImgSource = function() {
+    return this.imgSource;
+  }
+
+  this.getTitle = function() {
+    return this.sceneTitle;
+  }
+
+  this.getDesc = function() {
+    return this.sceneDesc;
+  }
 };
 
-test.onclick = changePicture;
+// Create an array for all scenes
+var scenes = new Array();
+
+// Push all the scenes
+for (var i = 0; i < sceneLength; i++) {
+  var pushScene = new SceneData(i, "This is scene " + i, "This is what happens in scene " + i, "url('/scenes/scene-" + i + ".jpg')")
+  scenes.push(pushScene);
+};
+
+// Current scene location
+var imageSwitch = 0;
+
+function loadNewScene(imageSwitch) {
+  // swap out sceneData
+  selectBG.style.backgroundImage = scenes[imageSwitch].getImgSource();
+  selectSceneTitle.innerHTML = scenes[imageSwitch].getTitle();
+  selectSceneDesc.innerHTML = scenes[imageSwitch].getDesc();
+  selectSceneNumber.innerHTML = imageSwitch + "/" + sceneLength;
+}
+
+// when changeScene() button is clicked
+function changeScene() {
+  console.log("currently: " + imageSwitch);
+  loadNewScene(imageSwitch);
+
+  // if imageSwitch is less than the sceneLength
+  if (imageSwitch < (sceneLength - 1)) {
+    //  ++
+    imageSwitch++;
+  } else {
+    // restart from zero
+    imageSwitch = 0;
+  };
+  console.log("Now: " + imageSwitch);
+};
+
+
+
+// call the changeScene function
+selectImgChange.onclick = changeScene;
